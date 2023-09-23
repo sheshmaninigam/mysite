d0b783d5487e5b7e9c2a89aec1b49d5760d4a867
sheshmaninigam <nigama451@gmail.com>
2023-09-23 12:20:31 +0530

    commit:4

--- car/urls.py
+++ car/urls.py
@@ -4,4 +4,7 @@ from car import views
 
 urlpatterns = [
     path("home/",views.index, name= "index" ),
+    path("about/",views.about, name= "about" ),
+    path("services/",views.services, name= "services" ),
+    path("contact/",views.contact, name= "contact" ),
 ]
--- car/views.py
+++ car/views.py
@@ -3,4 +3,16 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("This is my home page")
+    return render(request, "car/index.html")
+    # return HttpResponse("This is my home page")
+
+
+def about(request):
+    return HttpResponse("This is my about page")
+
+def services(request):
+    return HttpResponse("This is my services page")
+
+def contact(request):
+    return HttpResponse("This is my contact page")
+
